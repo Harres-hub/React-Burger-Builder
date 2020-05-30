802c0dc9c3e9c54c9bd68628c75f850839152d64
Harres <rh2962@columbia.edu>
2020-05-29 23:03:53 -0400

    Toolbar & SideDrawer

--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Model.css';
-import Aux from "../../../hoc/Aux";
-import Backdrop from './Backdrop/Backdrop';
+import Aux from "../../../hoc/Aux/Aux";
+import Backdrop from '../Backdrop/Backdrop';
 
-const model=(props)=>(
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modelClosed} />
-        <div 
-            style={{
-                transform: props.show? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-            className={classes.Model}>
-            {props.children}
-        </div>
-    </Aux>
-    
-);
+class Model extends Component{
+    shouldComponentUpdate(nextProps, nextState){
+            return nextProps.show !== this.props.show;
+    }
 
-export default model;
+    componentDidUpdate(){
+        console.log("Model did update");
+    }
+
+    render() {
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modelClosed} />
+                <div 
+                    style={{
+                        transform: this.props.show? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                    className={classes.Model}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+export default Model;
